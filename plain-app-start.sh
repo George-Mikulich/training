db99e4c676865eda8fc9a09210f985c2d84a6079
root <root@DESKTOP-U9L5KI3>
2024-04-09 09:41:46 +0300

    fix script

--- plain-app-start.sh
+++ plain-app-start.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 # This script installs argocd and deploys simple guestbook app
 
+set -e
+
 minikube start
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 brew install argocd
 argocd login --core
 kubectl config set-context --current --namespace=argocd
-argocd app create guestbook --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-server https://kubernetes.default.svc --dest-namespace default
+
+for (( ; ; ))
+do
+        echo "waiting for pods to be ready"
+        sleep 1
+        allPods=$(kubectl get pods --no-headers | wc -l)
+        runningPods=$(kubectl get pods --no-headers | grep -P "(\d+)\/\1\s+Running" | wc -l)
+        if [ $allPods == $runningPods ]
+        then
+                echo "all pods are ready"
+                break
+        fi
+done
+
+argocd app create guestbook --repo https://github.com/George-Mikulich/training --revision first-app-plain --path guestbook-app --dest-server https://kubernetes.default.svc --dest-namespace default
 argocd app sync guestbook
 minikube service guestbook-ui --url
